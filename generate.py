3c76f55990986e9544b274ae9fb51063c2f198a0
Andreas Rammhold <andreas@rammhold.de>
2016-10-29 03:06:45 +0200

    Pep8 fixes

--- generate.py
+++ generate.py
@@ -31,9 +31,11 @@ config_schema = {
 def writeable_dir(values):
     prospective_dir = values
     if not os.path.isdir(prospective_dir):
-        raise argparse.ArgumentTypeError("writeable_dir:{0} is not a valid path".format(prospective_dir))
+        raise argparse.ArgumentTypeError(
+            "writeable_dir:{0} is not a valid path".format(prospective_dir))
     if not os.access(prospective_dir, os.W_OK | os.R_OK):
-        raise argparse.ArgumentTypeError("writeable_dir:{0} is not a writeable dir".format(prospective_dir))
+        raise argparse.ArgumentTypeError(
+            "writeable_dir:{0} is not a writeable dir".format(prospective_dir))
     return prospective_dir
 
 
@@ -43,7 +45,9 @@ def prepare_resolvers(nameservers, loop=None):
 
     resolvers = {}
     for name, servers in nameservers.items():
-        resolvers[name] = list((server, aiodns.DNSResolver(loop=loop, nameservers=[server])) for server in servers)
+        resolvers[name] = list(
+            (server, aiodns.DNSResolver(loop=loop, nameservers=[server]))
+            for server in servers)
 
     return resolvers
 
@@ -63,7 +67,10 @@ def resolve_target(target, resolvers, loop):
     for host in target['hosts']:
         for name, r in resolvers.items():
             for resolver in r:
-                tasks.append(resolve_host(host, resolver[1], (host, name, resolver[0])))
+                tasks.append(
+                    resolve_host(
+                        host, resolver[1],
+                        (host, name, resolver[0])))
 
     results = {}
     for task in tasks:
@@ -86,15 +93,34 @@ def resolve_target(target, resolvers, loop):
 def generate_message(media, target, conf, result):
     if media in conf:
         template = Template(media.get(result))
-        return template.render(target=target, conf=conf, result=result, media=media)
+        return template.render(
+            target=target,
+            conf=conf,
+            result=result,
+            media=media)
     else:
         raise RuntimeError('Invalid media {} for {}'.format(media, target))
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--config', dest='config', default='conf.yaml', type=argparse.FileType('r'))
-    parser.add_argument('-l', '--log-level', dest='log_level', choices=['DEBUG','ERROR', 'INFO', 'WARN'], help='Debug level', default='INFO')
+    parser.add_argument(
+        '-c',
+        '--config',
+        dest='config',
+        default='conf.yaml',
+     type=argparse.FileType('r'))
+    parser.add_argument(
+        '-l',
+        '--log-level',
+        dest='log_level',
+        choices=[
+            'DEBUG',
+            'ERROR',
+            'INFO',
+            'WARN'],
+        help='Debug level',
+         default='INFO')
     parser.add_argument('dest', default='dist', type=writeable_dir)
 
     args = parser.parse_args()
@@ -123,14 +149,16 @@ def main():
                 for host, rs in result.items()
                 for resolver, servers in rs.items()
                 for server, success in servers.items()
-        ): msg = "some"
+        ):
+            msg = "some"
 
         if all(
                 success
                 for host, rs in result.items()
                 for resolver, servers in rs.items()
                 for server, success in servers.items()
-        ): msg = "all"
+        ):
+            msg = "all"
 
         results[name] = dict(hosts=result, summary=msg)
 
@@ -139,8 +167,13 @@ def main():
     jinja_env = Environment(loader=FileSystemLoader('templates/'))
     template = jinja_env.get_template('index.jinja2')
     with open(os.path.join(args.dest, 'index.html'), 'w') as fh:
-        fh.write(template.render(long_date=datetime.datetime.now().strftime('%B %Y'),
-                 results=results, targets=targets, messages=config['messages'], date=datetime.datetime.utcnow()))
+        fh.write(
+            template.render(
+                long_date=datetime.datetime.now().strftime('%B %Y'),
+                results=results,
+                targets=targets,
+                messages=config['messages'],
+                date=datetime.datetime.utcnow()))
 
 
 if __name__ == "__main__":
